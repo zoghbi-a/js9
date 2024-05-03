f4a0ebe37080944cb77c139b7e9d770c27177f6c
Abdu Zoghbi <a.zoghbi@nasa.gov>
2024-05-03 11:39:50 -0400

    fix analysis plugins

--- js9plugins.js
+++ js9plugins.js
@@ -15018,7 +15018,7 @@ JS9.RegisterPlugin(JS9.Toolbar.CLASS, JS9.Toolbar.NAME, JS9.Toolbar.init,
 		    help: "help/toolbar.html",
 		    winTitle: "Toolbar",
 		    winDims: [JS9.Toolbar.WIDTH, JS9.Toolbar.HEIGHT]});
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./imexam":[function(require,module,exports){
+var require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./imexam":[function(require,module,exports){
     /*jslint white: true, vars: true, plusplus: true, nomen: true, unparam: true, bitwise: true */
     /*globals Float32Array, Int32Array, JS9, $ */ 
 
